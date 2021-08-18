bc40c02e954a7200b5c1a5e069ad2bcf91f9870e
drakonhg <drakonhg@gmail.com>
2021-08-18 12:15:54 +0300

    Implemented python script

---
+++ .github/actions/test-telegram/script.py
@@ -0,0 +1,29 @@
+import os
+
+import requests
+
+# Environments for telegram bot
+TOKEN = os.environ['INPUT_TOKEN']
+CHAT_ID = os.environ['INPUT_TO']
+MESSAGE = os.environ['INPUT_MESSAGE']
+
+
+# Static request
+REQUEST = f'https://api.telegram.org/bot{TOKEN}/'
+
+
+def send_message(chat_id, message):
+    send_message_url = REQUEST + 'sendMessage'
+    send_message_data = {
+        'chat_id': chat_id,
+        'disable_web_page_preview': 'false',
+        'text': message.encode('utf-8')
+    }
+
+    response = requests.post(send_message_url, send_message_data)
+    print(response.json())
+    return response.json()
+
+
+if __name__ == '__main__':
+    send_message(CHAT_ID, MESSAGE)
