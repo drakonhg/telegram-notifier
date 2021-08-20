443325862d9ada0c70808183ba3e54c791667b92
drakonhg <drakonhg@gmail.com>
2021-08-20 18:02:43 +0300

    github-ci:Added README.md for all actions

--- .github/actions/telegram-notifier/README.md
+++ .github/actions/telegram-notifier/README.md
@@ -1,3 +1,21 @@
+#Send Telegram notifications on failures
+
+Action sends notifications to Telegram channel/bot. Action code is written in Python to be able to use docker container. 
+Github Action type, which can be used independently from different Github Actions workflows. 
+Also use of Python gives the ability to run the Action on any OS with Python installed. Action uses 'MarkdownV2' style for sending messages. Action has comment in Telegram message that it runs for failed jobs. Github environment variables printing also added to get more information from results log.
+
+Action sends message to single common TELEGRAM_TO Telegram channel for release Tarantool branches and tagged commits:
+
+- 1.10
+- 2.*
+- master
+- 'tagged versions' 
+  
+And sends all the others to <TELEGRAM_TO>_<USER>.
+
+
+## How to use Github Action from Github workflow
+```yaml
 name: Test Workflow Telegram Bot
 on: [push]
 jobs:
@@ -34,3 +52,11 @@ jobs:
           ---------------- Commit message -------------------\n
           "
           disable_web_page_preview: true
+ ```
+
+## Secrets
+
+Getting started with Telegram Bot API.
+
+- token: Telegram authorization token.
+- to: Unique identifier for this chat.
--- .github/actions/telegram-notifier/script.py
+++ .github/actions/telegram-notifier/script.py
@@ -7,20 +7,27 @@ TOKEN = os.environ['INPUT_TOKEN']
 CHAT_ID = os.environ['INPUT_TO']
 MESSAGE = os.environ['INPUT_MESSAGE']
 PAGE_PREVIEW = os.environ['INPUT_DISABLE_WEB_PAGE_PREVIEW']
-BRANCH_NAME = os.environ['BRANCH_NAME']
+BRANCH_NAME = os.environ['GITHUB_REF']
 
 # Static request
 REQUEST = f'https://api.telegram.org/bot{TOKEN}/'
 
 
-# def check_branch_name(branch_name):
-#     if branch_name != 'master' or branch_name != '1.10'
+def check_branch_name(branch_name):
+    branch_list = [
+        branch_name != 'refs/heads/master',
+        branch_name != 'refs/heads/1.10',
+        not branch_name.startswith('refs/heads/2.'),
+        not branch_name.startswith('refs/tags'),
+        ]
+
+    if any(branch_list):
+        return CHAT_ID + "_" + branch_name
+    return CHAT_ID
 
 def send_message(chat_id, message,
-                 disable_web_page_preview=False, markdown='MakrdownV2'):
+                 disable_web_page_preview=False, markdown='MarkdownV2'):
     edit_message = message.replace('-', '\\-').replace('_', '\\_')
-
-    print(os.environ["GITHUB_REF"])
 
     send_message_url = REQUEST + 'sendMessage'
     send_message_data = {
@@ -31,9 +38,10 @@ def send_message(chat_id, message,
     }
 
     response = requests.post(send_message_url, send_message_data)
-    print(response.json())
+
     return response.json()
 
 
 if __name__ == '__main__':
-    send_message(CHAT_ID, MESSAGE, PAGE_PREVIEW)
+    chat_id = check_branch_name(BRANCH_NAME)
+    send_message(chat_id, MESSAGE, PAGE_PREVIEW)
