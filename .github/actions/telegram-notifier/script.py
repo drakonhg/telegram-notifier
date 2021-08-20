ab6ac1e98ea9decc021e6b353a474817f1264b4c
drakonhg <drakonhg@gmail.com>
2021-08-20 18:17:56 +0300

    github-ci:Added README.md for all actions

--- .github/actions/telegram-notifier/script.py
+++ .github/actions/telegram-notifier/script.py
@@ -8,22 +8,23 @@ CHAT_ID = os.environ['INPUT_TO']
 MESSAGE = os.environ['INPUT_MESSAGE']
 PAGE_PREVIEW = os.environ['INPUT_DISABLE_WEB_PAGE_PREVIEW']
 BRANCH_NAME = os.environ['GITHUB_REF']
+ACTOR = os.environ['GITHUB_ACTOR']
 
 # Static request
 REQUEST = f'https://api.telegram.org/bot{TOKEN}/'
 
 
-# def check_branch_name(branch_name):
-#     branch_list = [
-#         branch_name != 'refs/heads/master',
-#         branch_name != 'refs/heads/1.10',
-#         not branch_name.startswith('refs/heads/2.'),
-#         not branch_name.startswith('refs/tags'),
-#         ]
-#
-#     if any(branch_list):
-#         return CHAT_ID + "_" + branch_name.split('/')[-1]
-#     return CHAT_ID
+def check_branch_name(branch_name):
+    branch_list = [
+        branch_name != 'refs/heads/master',
+        branch_name != 'refs/heads/1.10',
+        not branch_name.startswith('refs/heads/2.'),
+        not branch_name.startswith('refs/tags'),
+        ]
+
+    if any(branch_list):
+        return CHAT_ID + "_" + ACTOR
+    return CHAT_ID
 
 def send_message(chat_id, message,
                  disable_web_page_preview=False, markdown='MarkdownV2'):
@@ -38,10 +39,10 @@ def send_message(chat_id, message,
     }
 
     response = requests.post(send_message_url, send_message_data)
-
+    print(response.json())
     return response.json()
 
 
 if __name__ == '__main__':
-    # chat_id = check_branch_name(BRANCH_NAME)
-    send_message('@tntcore_ghactions_drakonhg', MESSAGE, PAGE_PREVIEW)
+    chat_id = check_branch_name(BRANCH_NAME)
+    send_message(chat_id, MESSAGE, PAGE_PREVIEW)
