391a59ca13fdf448c3b37628b815066f9b976ae4
drakonhg <drakonhg@gmail.com>
2021-08-20 16:49:20 +0300

    github-ci:Added Commit SHA check for later use

--- .github/actions/test-telegram/script.py
+++ .github/actions/test-telegram/script.py
@@ -7,22 +7,25 @@ TOKEN = os.environ['INPUT_TOKEN']
 CHAT_ID = os.environ['INPUT_TO']
 MESSAGE = os.environ['INPUT_MESSAGE']
 PAGE_PREVIEW = os.environ['INPUT_DISABLE_WEB_PAGE_PREVIEW']
-
+BRANCH_NAME = os.environ['BRANCH_NAME']
 
 # Static request
 REQUEST = f'https://api.telegram.org/bot{TOKEN}/'
 
 
+# def check_branch_name(branch_name):
+#     if branch_name != 'master' or branch_name != '1.10'
+
 def send_message(chat_id, message,
                  disable_web_page_preview=False, markdown='MakrdownV2'):
     edit_message = message.replace('-', '\\-').replace('_', '\\_')
 
-    print(os.environ["BRANCH_NAME"])
+    print(os.environ["GITHUB_REF"])
 
     send_message_url = REQUEST + 'sendMessage'
     send_message_data = {
         'chat_id': chat_id,
-        'parse_mode': 'MarkdownV2',
+        'parse_mode': markdown,
         'disable_web_page_preview': disable_web_page_preview,
         'text': edit_message,
     }
