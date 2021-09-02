0777c5607503d86daf3db698559caf9f58e2915f
drakonhg <drakonhg@gmail.com>
2021-09-02 19:05:09 +1000

    github-ci:Create a TypeScript Action

--- .github/actions/typescript-telegram-notifier/src/main.ts
+++ .github/actions/typescript-telegram-notifier/src/main.ts
@@ -1,15 +1,16 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import axios, { AxiosResponse } from "axios";
 
-(async function run(): Promise<void> {
+async function run() {
+  core.debug("Starting...");
+  const botToken = core.getInput("token");
+  const chatId = core.getInput("to");
+  const message = core.getInput("message");
+  const parseMode = core.getInput("parse_mode");
+  const disablePagePreview = core.getBooleanInput("disable_web_page_preview");
   try {
-    const botToken = core.getInput("token");
-    const chatId = core.getInput("to");
-    const message = core.getInput("message");
-    const parseMode = core.getInput("parse_mode");
-    const disablePagePreview = core.getBooleanInput("disable_web_page_preview");
-
-    const gitHubToken = core.getInput("github_token");
+    // const gitHubToken = core.getInput("github_token");
 
     core.debug(
       `Sending Message, payload=${JSON.stringify(github.context.payload)}`
@@ -25,25 +26,28 @@ import * as github from "@actions/github";
   } catch (error: any) {
     core.setFailed(error.message);
   }
-})();
+}
 
-async function _send_message<T>(
+
+async function _send_message(
   botToken: string,
-  chatId: string,
+  chatId: string | number,
   message: string,
   parseMode: string,
   disablePagePreview: boolean
-): Promise<T> {
+) {
+  core.debug("Making a Request");
   const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
-  const response = await fetch(url, {
-    method: "post",
-    body: JSON.stringify({
-      chat_id: chatId,
-      text: message,
-      parse_mode: parseMode,
-      disable_web_page_preview: disablePagePreview,
-    }),
-  });
-
-  return response.json();
+  core.debug(url)
+  axios.post(url, {
+    chat_id: chatId,
+    text: message,
+    parse_mode: parseMode,
+    disable_web_page_preview: disablePagePreview,
+  })
+      .then(res => console.log(res.data))
+      .catch(err => core.debug(err))
 }
+
+
+run()
